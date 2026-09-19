c4f293dc625918476848d7daee2ea9f3ba41bd29
thig0S_ <thiagokovalski5@gmail.com>
2026-09-18 22:36:55 -0300

    alteracao no saveChangesAsync para cadastrar com o id do usuario

--- src/Infraestrutura/Modulos/Cursos/RepositorioCursoEmOrm.cs
+++ src/Infraestrutura/Modulos/Cursos/RepositorioCursoEmOrm.cs
@@ -1,10 +1,11 @@
 using GeradorCertificados.Dominio.Compartilhado;
+using GeradorCertificados.Dominio.Compartilhado.Auth;
 using GeradorCertificados.Dominio.Modulos.ModuloCurso;
 using GeradorCertificados.Infraestrutura.Compartilhado.Orm;
 
 namespace GeradorCertificados.Infraestrutura.Modulos.Cursos;
 
-public class RepositorioCursoEmOrm(GeradorCertificadosDbContext dbContext) : RepositorioBaseEmOrm<Curso>(dbContext), IRepositorioCurso
+public class RepositorioCursoEmOrm(GeradorCertificadosDbContext dbContext, IProvedorDeUsuario provedorDeUsuario)
+     : RepositorioBaseEmOrm<Curso>(dbContext, provedorDeUsuario), IRepositorioCurso
 {
-
 }
